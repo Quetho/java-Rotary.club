7bcea1dc2b618e9094b113e63c921b60acee6b17
Quentin THOMAS <q.thomas@live.com>
2017-01-24 15:00:50 +0100

    All parts defined

---
+++ src/modPerf.java
@@ -0,0 +1,30 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aeons
+ */
+public class modPerf extends mods{
+    ///SURALIM///
+    ArrayList<String> suralim =  
+            new ArrayList<String>(Arrays.asList("stock","turbo","compresseur"));
+    ///
+    
+    ///exhaust///
+    ArrayList<String> echap =  
+            new ArrayList<String>(Arrays.asList("stock","hks","magnaflow","greddy"));
+    ///
+    
+    ///PNEU///
+    ArrayList<String> pneu =  
+            new ArrayList<String>(Arrays.asList("stock","michelin","toyo","pzero"));
+    ///
+}
