6e08937095658ddc415713c3e4547c76d3ba4147
Quentin THOMAS <q.thomas@live.com>
2017-01-24 15:08:08 +0100

    resolving missing files

---
+++ src/modInt.java
@@ -0,0 +1,30 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aeons
+ */
+public class modInt extends mods{
+    ///VOLANT///
+    ArrayList<String> volant =  
+            new ArrayList<String>(Arrays.asList("stock","nardi","momo","sparco"));
+    ///
+    
+    ///SIEGE///
+    ArrayList<String> siege =  
+            new ArrayList<String>(Arrays.asList("stock","bride","sparco","ikea"));
+    ///
+    
+    ///FAM///
+    ArrayList<String> fam =  
+            new ArrayList<String>(Arrays.asList("stock","hydrolique","custom","feuvert"));
+    ///
+}
