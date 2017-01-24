e77378e2bd764a415626946aa81f3fb176c32810
Quentin THOMAS <q.thomas@live.com>
2017-01-24 14:24:42 +0100

    Menu simple fonctionnel

--- src/RotaryClubMain.java
+++ src/RotaryClubMain.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,6 +18,41 @@ public class RotaryClubMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Rotary.Club Garage");
+        garage g = new garage();
+        vehiculeToMod v = new vehiculeToMod();
+        
+        int choix;
+        do{
+        System.out.println("---] MENU [---");
+        System.out.println("------------");
+        System.out.println("->[1]Tous les vehicules.");
+        System.out.println("->[2]Les vehicules à modifier.");
+        System.out.println("->[3]Les vehicules prets.");
+        System.out.println("------------");
+        System.out.print("Votre choix ? ");
+        Scanner reader = new Scanner(System.in);
+        choix = reader.nextInt();
+        String menurep1 = "Tous les vehicules";
+        String menurep2 = "Les vehicules à modifier";
+        String menurep3 = "Les vehicules prets";
+        
+        
+        switch(choix){
+            case 1:
+            System.out.println(menurep1);
+                break;
+            case 2:
+            System.out.println(menurep2);
+                break;
+            case 3:
+            System.out.println(menurep3);
+                break;
+            default :
+                System.out.println("["+choix+"] n'est pas une entrée valide.");
+        }
+        }while( choix != 0);
     }
     
 }
+///reader.nextLine()   ->->-> chaine de char
