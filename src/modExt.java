78e88db6c610822f2da78db74626e01ba5610062
Quentin THOMAS <q.thomas@live.com>
2017-01-24 14:48:13 +0100

    ethetic parts defined

---
+++ src/modExt.java
@@ -0,0 +1,31 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aeons
+ */
+public class modExt extends mods {
+    
+    ///PEINTURE/// 
+    ArrayList<String> peinture =  
+            new ArrayList<String>(Arrays.asList("blanc","bleu","violet","rouge","orange","jaune","vert","noir"));
+    ///
+   
+    ///CAPOT///
+    ArrayList<String> capot =  
+            new ArrayList<String>(Arrays.asList("metalStock","metalEvo","metalR34","metalRx7","carbonStock","carbonEvo","carbonR34","carbonRx7"));
+    ///
+    
+    ///SPOILER///
+    ArrayList<String> spoiler =  
+            new ArrayList<String>(Arrays.asList("metalStock","metalEvo","metalR34","metalRx7","carbonStock","carbonEvo","carbonR34","carbonRx7"));
+    ///
+}
